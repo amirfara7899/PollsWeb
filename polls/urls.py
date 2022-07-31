5dc84f1c76d5610916ef199b8e653167566b9a1f
amirfara7899 <amir.faramarzi.7899@gmail.com>
2022-07-31 13:37:09 +0430

    add detail,results and vote urls poll functions

--- polls/urls.py
+++ polls/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index),
+    path('detail/<question_id>', views.detail),
+    path('results/<question_id>', views.results),
+    path('vote/<question_id>', views.vote),
 ]
