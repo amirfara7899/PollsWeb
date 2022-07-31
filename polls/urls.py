f319e75804844b8fb55720839fa527ae24e578af
amirfara7899 <amir.faramarzi.7899@gmail.com>
2022-07-31 13:51:03 +0430

    fix default polls url

--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('' or 'index/', views.index),
+    path('', views.index),
+    path('index/', views.index),
     path('detail/<question_id>', views.detail),
     path('<question_id>/results', views.results),
     path('<question_id>/vote', views.vote),
