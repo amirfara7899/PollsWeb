c4630a79eb9a08dadff5ec877d8d11ed40d0db43
amirfara7899 <amir.faramarzi.7899@gmail.com>
2022-07-31 13:26:31 +0430

    add detail, result and vote function in polls view

--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("This is the index page.")
+
+
+def detail(request, question_id):
+    return HttpResponse("This is the detail view of the question: $s" % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse("This is the results view of the question: $s" % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("Vote on question: $s" % question_id)
