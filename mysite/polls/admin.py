b4b289a4efa3cc682693139cb397e1651ad13e18
amirfara7899 <amir.faramarzi.7899@gmail.com>
2022-07-31 12:39:51 +0430

    add question and choice object to admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Question, Choice
+
 
 # Register your models here.
+
+class QuestoinAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date')
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('choice_text', 'votes', 'question')
+
+
+admin.site.register(Question, QuestoinAdmin)
+admin.site.register(Choice, ChoiceAdmin)
