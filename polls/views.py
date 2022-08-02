100a0b992e50ba37d935ad8ceffcbb84f71fd3f1
amirfara7899 <amir.faramarzi.7899@gmail.com>
2022-08-02 21:09:15 +0430

    update vote view for update choices

--- polls/views.py
+++ polls/views.py
@@ -19,4 +19,14 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("Vote on question:" + str(question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except:
+        return render(request, 'polls/detail.html',
+                      {'question': question, 'error_message': "Please select a choice"})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
