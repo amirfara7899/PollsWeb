334af35c0a75d5258ffc4a2a6d97d4fe712ef8d2
amirfara7899 <amir.faramarzi.7899@gmail.com>
2022-08-02 12:49:38 +0430

    add get_object_or_404 to detail view

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.shortcuts import render
 from .models import Question
 
 
@@ -10,7 +10,7 @@ def index(request):
 
 
 def detail(request, question_id):
-    question = Question.objects.get(pk=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 
