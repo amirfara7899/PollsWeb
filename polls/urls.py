f0c747a7743fabcc2b17dcacfd7a696d43fb6191
amirfara7899 <amir.faramarzi.7899@gmail.com>
2022-08-02 20:18:05 +0430

    update polls url and make it more readable

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index),
-    path('index/', views.index),
-    path('detail/<question_id>', views.detail),
-    path('<question_id>/results', views.results),
-    path('<question_id>/vote', views.vote),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
